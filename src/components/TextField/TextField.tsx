eddaed6e5b72cd367b7f8617f40844e653537ced
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-21 16:55:57 +0200

    Create TextField component

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  text: string;
+}
+
+const TextField: React.FC<Props> = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+}
+
+export default TextField;
