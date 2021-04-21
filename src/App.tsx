071c2eb330067db3d4fad12d6309ced502435901
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-21 17:03:24 +0200

    Implement TextField in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import TextField from './components/TextField/TextField';
+
 import './App.css';
 
 const App: React.FC = () => (
   <div className="App">
       <h1>My First ts React App</h1>
+      <TextField 
+        text='hello' 
+        person={
+          {
+            firstName: 'Grzegorz',
+            lastName: 'Swieboda'
+          }
+        }
+      />
   </div>
 );
 
