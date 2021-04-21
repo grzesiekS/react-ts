0125583d77de198b81560dc5eea9576afc09ad37
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-04-21 17:09:53 +0200

    Add hooks in TextField component

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Person {
   firstName: string;
@@ -11,9 +11,20 @@ interface Props {
   person: Person;
 }
 
-const TextField: React.FC<Props> = () => {
+interface Title {
+  text: string;
+}
+
+const TextField: React.FC<Props> = ({ text, person, fn }) => {
+  const [title, setTitle] = useState<Title>({ text: 'Hello World!' });
+
+  useEffect(() => {
+    setTitle({ text: `Hello ${person.firstName} ${person.lastName}` });
+  }, [person.firstName, person.lastName])
+
   return (
     <div>
+      <h2>{title.text}</h2>
       <input />
     </div>
   );
